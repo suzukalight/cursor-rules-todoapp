59065af33af76bd39aed19fc15f726bb9bdb7b6a
suzukalight <mail@mkubara.com>
2025-01-26 16:31:49 +0900

    feat(ui): Add Select component exports to UI package
    - Export Select component and related subcomponents from new select module
    - Create index file for Select component in UI package
    - Update main UI package index to include Select component exports

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -5,4 +5,14 @@ export * from './components/checkbox';
 export * from './components/label';
 export * from './components/textarea';
 export * from './components/card';
-export * from './components/toast'; 
+export * from './components/toast';
+export {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from './components/select/select'; 
